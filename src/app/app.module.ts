08ab75545fede0276d374485c44330a999bb3955
unknown <danieldebrito@outlook.com>
2019-06-23 04:56:38 -0300

    forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
 // styles
@@ -20,6 +21,7 @@ import { ToasterComponent } from './components/toaster/toaster.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     BrowserAnimationsModule,
     ToastrModule.forRoot()
   ],
